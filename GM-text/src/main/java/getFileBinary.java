16a090b171c5342cb360cc0f5f1c55238e0fc198
wql <2294336102@qq.com>
2023-10-11 15:08:03 +0800

    第一次提交后端

---
+++ GM-text/src/main/java/getFileBinary.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+/**
+ * @author abyss
+ * @version 1.0-SNAPSHOT
+ * @className getFileBinary
+ * @description TODO
+ * @date 11/10/2023
+ */
+public class getFileBinary {
+
+}
