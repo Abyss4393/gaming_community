2d187ef0ec0f27afae76c867e223ddcc00fdf444
wql <2294336102@qq.com>
2023-10-14 16:51:04 +0800

    增加心跳时间

--- src/utils/websocket/index.js
+++ src/utils/websocket/index.js
@@ -19,7 +19,7 @@ export const initws = function (url, toId) {
 
 // WebSocket心跳检测
 export var ws_heartCheck = {
-    timeout: 5000,			// 5秒一次心跳
+    timeout: 1000 * 60 * 5,			// 10秒一次心跳
     timeoutObj: null,		// 执行心跳的定时器
     serverTimeoutObj: null,	// 服务器超时定时器
     reset: function () {		// 重置方法
@@ -28,15 +28,18 @@ export var ws_heartCheck = {
         return this;
     },
     start: function (socket) {		// 启动方法
-        var self = this;
-        this.timeoutObj = setTimeout(function () {
-            // 这里发送一个心跳信息，后端收到后，返回一个消息，在onmessage拿到返回的心跳（信息）就说明连接正常
-            socket.send("waiting");
+        var _this = this;
+        _this.timeoutObj = setTimeout(function () {
+            if (socket.readyState === WebSocket.OPEN) {
+                // 这里发送一个心跳信息，后端收到后，返回一个消息，在onmessage拿到返回的心跳（信息）就说明连接正常
+                socket.send("waiting");
+                _this.reset().start(socket);
+            }
             // 如果超过一定时间还没重置，说明后端主动断开了
             self.serverTimeoutObj = setTimeout(function () {
                 // 如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
                 socket.close();
-            }, self.timeout);
-        }, this.timeout);
+            }, _this.timeout);
+        }, _this.timeout);
     }
 }
