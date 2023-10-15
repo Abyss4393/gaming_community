55133fcc0e9d70b9130674db7d00aca49a607c76
wql <2294336102@qq.com>
2023-10-15 22:19:35 +0800

    解析器

---
+++ GM-websocket/GM-websocket-service/src/main/java/cn/abyss4393/webservice/utils/WebSocketMessageDecoder.java
@@ -0,0 +1,14 @@
+package cn.abyss4393.webservice.utils;
+
+/**
+ * @author abyss
+ * @version 1.0-SNAPSHOT
+ * @className MessageDecoder
+ * @description TODO
+ * @date 13/10/2023
+ */
+public class WebSocketMessageDecoder {
+    public static void decoder(String binary){
+
+    }
+}
