8bfd9b82ec208435b9553da0c5b1c31e7e6f392d
wql <2294336102@qq.com>
2023-10-16 19:47:53 +0800

    无

--- GM-websocket/GM-websocket-service/src/main/java/cn/abyss4393/webservice/utils/WebSocketMessageDecoder.java
+++ GM-websocket/GM-websocket-service/src/main/java/cn/abyss4393/webservice/utils/WebSocketMessageDecoder.java
@@ -1,5 +1,7 @@
 package cn.abyss4393.webservice.utils;
 
+import cn.hutool.core.codec.Base64Decoder;
+
 /**
  * @author abyss
  * @version 1.0-SNAPSHOT
@@ -8,7 +10,8 @@ package cn.abyss4393.webservice.utils;
  * @date 13/10/2023
  */
 public class WebSocketMessageDecoder {
-    public static void decoder(String binary){
+    public static byte[] decoder(String base64){
 
+        return Base64Decoder.decode(base64);
     }
 }
