3bc4718d18a5192475976c99514ea5763fbeb439
wql <2294336102@qq.com>
2023-10-17 23:09:46 +0800

    无

--- src/utils/request/common.js
+++ src/utils/request/common.js
@@ -2,11 +2,15 @@ import instance from "./index";
 
 
 //user
-export const LoginAPI = (params) => instance.post("user/login", params);
+const LoginAPI = (params) => instance.post("user/login", params);
+const UserInfo = (params) => instance.get(`user/info?uid=${params}`)
+const AbatinFriendListAPI = (params) => instance.get(`user/friends/${params}`)
 
 
-export const UserInfo = (params) => instance.get(`user/info?uid=${params}`)
-export const AbatinFriendListAPI = (params) => instance.get(`user/friends/${params}`)
+
+// article
+export const AsyncArticleList = () => instance.get('/common/article/list');
+
 //file
 const UploadAPI = (url) => instance.post(`file/upload`, url);
 const DelFileAPI = (rootFileUrl) => instance.delete(`file/delete`, rootFileUrl)
