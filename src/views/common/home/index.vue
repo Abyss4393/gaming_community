31a53c5d5abb1dfdf58c6ac2da40bf8f3a8c5e9a
wql <2294336102@qq.com>
2023-10-17 23:11:18 +0800

    无

--- src/views/common/home/index.vue
+++ src/views/common/home/index.vue
@@ -1,26 +1,74 @@
 <template>
-    <div id="abyss">
-        <abyssHeader />
-        <router-view />
-        <abyssFooter />
-        <div class="abyss-top" v-if="data.scroll > 300">
-            <img @click="back" :src="require('@/assets/static/icons/top.png')" alt="">
+    <div class="container">
+        <div class="container-main">
+            <div class="article-list-container">
+                <div class="article-list-body">
+                    <el-card class="aticle-item-card" shadow="hover" v-for="item in data.articleList" :key="item.id">
+                        <div class="article-item-card-header">
+                            <div class="article-item-header-avatar">
+                                <img :src="item.posterData.avatar" alt="">
+                            </div>
+                            <div class="article-item-header-nickname">
+                                <span>{{ item.posterData.nickname }}</span>
+                            </div>
+                            <div class="article-item-header-time">
+                                <span> {{ item.postTime.slice(0, 10) }}</span>
+                            </div>
+                        </div>
+                        <a href="/group" target="_blank">
+                            <div class="article-item-card-title">
+                                <h5>{{ item.type }}</h5>
+                                <h3 :title="item.title">{{ item.title }}</h3>
+                            </div>
+                            <div class="article-item-card-content">
+                                {{ item.content }}
+                            </div>
+                            <div class="article-item-card-preview">
+                                preview
+                            </div>
+                        </a>
+                        <div class="article-item-card-footer">
+                        </div>
+                    </el-card>
+                    <el-card class="load-more" v-if="data.loadingMore">
+                        <el-button type="primary" :text="true" @click="loadingMore">点击加载更多</el-button>
+                    </el-card>
+                </div>
+            </div>
+        </div>
+        <div class="container-sub">
+            gwgeggqgqgg
         </div>
     </div>
 </template>
 <script setup>
 import { reactive, onMounted, onUnmounted } from 'vue';
-import abyssHeader from '@/components/layout/abyss-navigation-header/index'
-import abyssFooter from '@/components/layout/abyss-navigation-footer/index'
+import { ElCard, ElButton } from 'element-plus';
+import { AsyncArticleList } from '@/utils/request/common.js'
+
+
 const data = reactive({
-    scroll: 0
+    articleList: [],
+    loadingMore: true
 })
-const doScroll = function (e) {
-    data.scroll = document.documentElement.scrollTop;
+onMounted(async () => {
+    const res = await AsyncArticleList();
+    if (res.meta.code === 200) {
+        data.articleList = res.data
+        if (data.articleList.length > 2)
+            data.articleList.splice(2, data.articleList.length - 1);
+    }
+})
+
+const loadingMore = async () => {
+    data.loadingMore = false;
+    const res = await AsyncArticleList();
+    if (res.meta.code === 200) {
+        data.articleList = res.data
+    } else
+        data.loadingMore = true;
+
 }
-const back = () => document.documentElement.scrollTop = 0;
-onMounted(() => window.addEventListener("scroll", doScroll))
-onUnmounted(() => window.removeEventListener("scroll", doScroll))
 </script>
 <style lang="less" scoped>
 @import url('./index.less');
