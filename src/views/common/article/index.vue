e00131054d6d4349980f3a7fee71361a5903f7f3
wql <2294336102@qq.com>
2023-10-17 23:10:37 +0800

    增加article

---
+++ src/views/common/article/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container">
+        <div class="container-main">
+            <div class="article-list-container">
+                <div class="article-list-body">
+                    <el-card class="aticle-item-card" shadow="hover" v-for="item in data.articleList" :key="item.id">
+                        <div class="article-item-card-header">
+                            <div class="article-item-header-avatar">
+                                <img :src="item.posterData.avatar" alt="">
+                            </div>
+                            <div class="article-item-header-nickname">
+                                <span>{{ item.posterData.nickname }}</span>
+                            </div>
+                            <div class="article-item-header-time">
+                                <span> {{ item.postTime.slice(0, 10) }}</span>
+                            </div>
+                        </div>
+                        <a href="/group" target="_blank">
+                            <div class="article-item-card-title">
+                                <h5>{{ item.type }}</h5>
+                                <h3 :title="item.title">{{ item.title }}</h3>
+                            </div>
+                            <div class="article-item-card-content">
+                                {{ item.content }}
+                            </div>
+                            <div class="article-item-card-preview">
+                                preview
+                            </div>
+                        </a>
+                        <div class="article-item-card-footer">
+                        </div>
+                    </el-card>
+                    <el-card class="load-more">
+                        <el-button type="primary" :text="true" @click="loadingMore">点击加载更多</el-button>
+                    </el-card>
+                </div>
+            </div>
+        </div>
+        <div class="container-sub">
+            gwgeggqgqgg
+        </div>
+    </div>
+</template>
+<script setup>
+import { reactive, onMounted, onUnmounted } from 'vue';
+import { ElCard, ElButton } from 'element-plus';
+import { AsyncArticleList } from '@/utils/request/common.js'
+
+
+const data = reactive({
+    articleList: []
+})
+onMounted(async () => {
+    const res = await AsyncArticleList();
+    if (res.meta.code === 200) {
+        data.articleList = res.data
+        if (data.articleList.length > 2)
+            data.articleList.splice(2, data.articleList.length -1);
+    }
+})
+
+const loadingMore = async () => {
+    const res = await AsyncArticleList();
+    if (res.meta.code === 200) {
+        data.articleList = res.data
+    }
+}
+</script>
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
