dfef8f4a4d7e86968fb36a1e4d30f01a165339e8
wql <2294336102@qq.com>
2023-10-20 16:02:00 +0800

    发布帖子

---
+++ src/views/module/new/new-article/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="abyss-new-article">
+        <rich-text></rich-text>
+    </div>
+</template>
+<script setup>
+import { getCurrentInstance, onMounted } from 'vue';
+import richText from '@/components/rich-text/index'
+
+
+const instance = getCurrentInstance();
+
+const uid = instance.proxy.$route.query['author_id'];
+
+
+</script>
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
