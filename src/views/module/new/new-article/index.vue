7de68a31dfba2654e86ab1d7d4f51b1615b5c853
wql <2294336102@qq.com>
2023-10-21 14:33:39 +0800

    完善发布帖子部分

--- src/views/module/new/new-article/index.vue
+++ src/views/module/new/new-article/index.vue
@@ -4,31 +4,34 @@
             <el-card class="content">
                 <span class="crumbs">> 发布帖子</span>
                 <div class="editor">
-                    <el-form ref="form" :model="data.article" :rules="rules">
+                    <el-form ref="ArticleForm" :model="data.article" :rules="rules">
                         <el-row>
-                            <el-col :span="4" :push="2"><span>标题:</span></el-col>
-                            <el-col :span="16">
+                            <el-col :span="2"><span>标题:</span></el-col>
+                            <el-col :span="22">
                                 <el-form-item prop="title">
                                     <el-input type="text" placeholder="标题(必填)" v-model="data.article.title" maxlength="50"
                                         show-word-limit />
                                 </el-form-item>
                             </el-col>
                         </el-row>
-                        <el-row>
-                            <el-col :span="4" :push="2"><span>内容:</span></el-col>
-                            <el-col :span="16">
-                                <!-- <rich-text></rich-text> -->
+                        <el-row style="height: 20rem;">
+                            <el-col :span="2"><span>内容:</span></el-col>
+                            <el-col :span="22">
+                                <quill-editor v-model:value="data.quillEditor.content"
+                                    :options="data.quillEditor.editorOption" :disabled="data.quillEditor.disabled"
+                                    @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+                                    @ready="onEditorReady($event)" @change="onEditorChange($event)" />
                             </el-col>
                         </el-row>
-                        <el-row>
-                            <el-col :span="2" :push="2"><span>类型:</span></el-col>
-                            <el-col :span="6" :push="4">
+                        <el-row style="margin-top: 5rem;">
+                            <el-col :span="2"><span>类型:</span></el-col>
+                            <el-col :span="7">
                                 <el-radio v-model="data.article.type" label="资讯">资讯</el-radio>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col :span="7" :pull="4">
                                 <el-radio v-model="data.article.type" label="攻略">攻略</el-radio>
                             </el-col>
-                            <el-col :span="6" :pull="4">
+                            <el-col :span="8" :pull="8">
                                 <el-radio v-model="data.article.type" label="新奇">新奇</el-radio>
                             </el-col>
                         </el-row>
@@ -42,13 +45,13 @@
     </div>
 </template>
 <script setup>
-import { getCurrentInstance, onMounted, reactive, ref } from 'vue';
+import { getCurrentInstance, h, reactive, ref } from 'vue';
 import { ElCard, ElForm, ElRow, ElCol, ElRadio, ElButton } from 'element-plus';
-import richText from '@/components/rich-text/index'
+import { quillEditor } from 'vue3-quill'
 
 
 const instance = getCurrentInstance();
-const form = ref(null);
+const ArticleForm = ref(null);
 
 const uid = instance.proxy.$route.query['author_id'];
 
@@ -57,6 +60,30 @@ const data = reactive({
         title: '',
         content: '',
         type: ''
+    },
+    quillEditor: {
+        disabled: false,
+        content: '',
+        editorOption: {
+            placeholder: '有时，jing',
+            modules: {
+                toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['blockquote'],
+                    [{ header: 1 }, { header: 2 }],
+                    [{ indent: '-1' }, { indent: '+1' }],
+                    [{ direction: 'rtl' }],
+                    [{ size: ['small', false, 'large', 'huge'] }],
+                    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                    [{ color: [] }, { background: [] }],
+                    [{ font: [] }],
+                    [{ align: [] }],
+                    ['clean'],
+                    ['link', 'image', 'video']
+                ]
+            },
+        },
+
     },
     confirm: false
 })
@@ -75,6 +102,27 @@ const rules = {
         { validator: validateTitle, trigger: 'blur' }
     ],
 };
+
+
+const onEditorBlur = (quill) => {
+    console.log('blur', quill);
+}
+
+const onEditorFocus = (quill) => {
+    console.log('focus', quill);
+
+}
+
+function onEditorReady({ quill, html, text }) {
+    console.log('ready');
+    console.log(quill, html, text);
+
+}
+
+function onEditorChange(e) {
+    console.log('change');
+    console.log(data.quillEditor.content);
+}
 
 
 function confirm() {
