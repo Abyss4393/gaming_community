885914f8b4f32eea3f6801a3c83e6a0c50e47a0e
wql <2294336102@qq.com>
2023-10-21 14:33:13 +0800

    完善发布图片部分

--- src/views/module/new/new-image/index.vue
+++ src/views/module/new/new-image/index.vue
@@ -6,17 +6,56 @@
                 <div class="editor">
                     <el-form ref="form" :model="data.image" :rules="rules">
                         <el-row>
-                            <el-col :span="4" :push="2"><span>标题:</span></el-col>
-                            <el-col :span="16">
+                            <el-col :span="2" :push="2"><span>标题:</span></el-col>
+                            <el-col :span="18" :push="2">
                                 <el-form-item prop="title">
                                     <el-input type="text" placeholder="标题(必填)" v-model="data.image.title" maxlength="50"
                                         show-word-limit />
                                 </el-form-item>
                             </el-col>
                         </el-row>
+                        <el-row>
+                            <el-col :span="2" :push="2"><span>描述</span>:</el-col>
+                            <el-col :span="18" :push="2">
+                                <div class="descript">
+                                    <div class="recommand">
+                                        <p>@</p>
+                                    </div>
+                                    <el-input :autosize="{ minRows: 5, maxRows: 25 }" type="textarea" placeholder="输入图片描述"
+                                        v-model="data.image.descript" maxlength="500" show-word-limit />
+                                </div>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="4" :push="2"><span>上传图片</span>:</el-col>
+                            <el-col :span="16" :push="2">
+                                <div class="upload">
+                                    <el-upload :action="_actionURL" :before-upload="beforeUpload"
+                                        :on-success="uploadSuccess" :on-preview="handlePreview" :on-remove="handleRemove"
+                                        :file-list="data.files" list-type="picture" multiple>
+                                        <template #="tip">
+                                            <el-button size="small" type="primary">点击上传</el-button>
+                                            <div>支持png、jpg、jpeg、gif图片格式，但图片大小不超过5MB</div>
+                                        </template>
+                                    </el-upload>
+                                </div>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="2" :push="2"><span>类型:</span></el-col>
+                            <el-col :span="7" :push="2">
+                                <el-radio v-model="data.image.type" label="资讯">资讯</el-radio>
+                            </el-col>
+                            <el-col :span="7" :pull="2">
+                                <el-radio v-model="data.image.type" label="攻略">攻略</el-radio>
+                            </el-col>
+                            <el-col :span="8" :pull="6">
+                                <el-radio v-model="data.image.type" label="新奇">新奇</el-radio>
+                            </el-col>
+                        </el-row>
                     </el-form>
                     <div class="confirm">
-                       
+                        <el-button type="primary" round :loading="data.confirm" @click="confirm">发布</el-button>
                     </div>
                 </div>
             </el-card>
@@ -25,14 +64,48 @@
 </template>
 <script setup>
 import { reactive } from 'vue';
+import { ElCard, ElForm, ElRow, ElCol, ElRadio, ElButton, ElMessage } from 'element-plus';
+
+const _actionURL = 'http://localhost:2766/api/private/v1/community/file/upload';
 const data = reactive({
-    image:{
-        title:''
-    }
+    image: {
+        title: '',
+        descript: '',
+        type: '',
+        list: [],
+
+
+    },
+    files: []
 })
 
 const rules = {
 
+}
+
+const beforeUpload = (file) => {
+    const type = file.type.match('image/');
+    const size = file.size / 1024 / 1024 < 5;
+    if (!type) ElMessage.error('上传文件仅支持图片格式');
+    if (!size) ElMessage.error('上传图片大于5MB')
+    return type && size;
+}
+
+const uploadSuccess = (res, file, fileList) => {
+    if (res.meta.code === 200) {
+        data.image.list.push(res.data.content['download_url']);
+        ElMessage.success('上传成功！')
+    }
+    else ElMessage.error('上传失败');
+
+}
+
+const handlePreview = (file) => {
+    console.log("preview", file);
+}
+
+const handleRemove = (file) => {
+    console.log("remove", file);
 }
 </script>
 <style lang="less" scoped>
