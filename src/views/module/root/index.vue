94cc2e70fd4ca479f44eeebd626363b906e2e0f0
wql <2294336102@qq.com>
2023-10-25 16:41:29 +0800

    置顶

--- src/views/module/root/index.vue
+++ src/views/module/root/index.vue
@@ -3,26 +3,17 @@
         <abyssHeader />
         <router-view />
         <abyssFooter />
-        <div class="abyss-top" v-if="data.scroll > 300">
-            <img @click="back" :src="require('@/assets/static/icons/top.png')" alt="">
-        </div>
+        <el-backtop>
+            <div class="abyss-top" :bottom="100">
+                <img @click="back" :src="require('@/assets/static/icons/top.png')" alt="">
+            </div>
+        </el-backtop>
     </div>
 </template>
 <script setup>
-import { reactive, onMounted, onUnmounted } from 'vue';
+import { ElBacktop } from 'element-plus';
 import abyssHeader from '@/components/layout/abyss-navigation-header/index'
 import abyssFooter from '@/components/layout/abyss-navigation-footer/index'
-
-
-const data = reactive({
-    scroll: 0,
-})
-const doScroll = function (e) {
-    data.scroll = document.documentElement.scrollTop;
-}
-const back = () => document.documentElement.scrollTop = 0;
-onMounted(async () => window.addEventListener("scroll", doScroll));
-onUnmounted(() => window.removeEventListener("scroll", doScroll));
 </script>
 <style lang="less" scoped>
 @import url('./index.less');
