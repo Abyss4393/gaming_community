a61184685d63922ed956b9734e290aaec47f7217
wql <2294336102@qq.com>
2023-11-01 15:59:53 +0800

    增加我的收藏

---
+++ src/views/module/account-center/my-collection/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="abyss-my-collection">
+        <div class="header">
+            我的收藏
+        </div>
+        <div class="list-body" v-if="data.list.length !== 0">
+            <ul>
+                <li v-for="item, index in data.list" :key="index">
+                    <a :href="`/abyss/article/${item.id}`" target="_blank">
+                        <div class="_inner">
+                            <span>发布时间 {{ item.postTime.slice(0, 10) }}</span>
+                        </div>
+                        <div class="_main">
+                            <h2>{{ item.title }}</h2>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div v-else>
+            <el-empty description="你还没有任何收藏的帖子，再去逛逛吧！" />
+        </div>
+    </div>
+</template>
+<script setup>
+import { useRoute } from 'vue-router';
+import { reactive } from 'vue';
+import { ElEmpty } from 'element-plus';
+import { AsyncUserCollections } from '@/utils/request/common.js'
+
+const uid = useRoute().query['author_id'];
+
+const data = reactive({
+    list: []
+});
+
+async function init() {
+    const res = await AsyncUserCollections(uid);
+    if (res.meta.code === 200) {
+        if (res.data != null)
+            data.list = res.data;
+    }
+}
+init();
+</script>
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
