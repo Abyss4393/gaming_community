bc33285431200133a4e7bfae6844fd436396c106
wql <2294336102@qq.com>
2023-11-15 14:01:33 +0800

    测试接口，ftp上传文件接口

--- GM-service-dto/src/main/java/cn/abyss4393/dto/controller/fileController/FileController.java
+++ GM-service-dto/src/main/java/cn/abyss4393/dto/controller/fileController/FileController.java
@@ -3,9 +3,7 @@ package cn.abyss4393.dto.controller.fileController;
 import cn.abyss4393.annotation.AuthAccess;
 import cn.abyss4393.entity.ResultFul;
 import cn.abyss4393.service.impl.FileServiceImpl;
-import cn.abyss4393.utils.imgbed.ImageBedUtils;
 import cn.abyss4393.utils.imgbed.ImagePath;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -35,7 +33,12 @@ public class FileController {
         return fileService.upload(file);
     }
 
-    ;
+    @AuthAccess(desc = "ftp上传音频/视频")
+    @PostMapping("/upload/ftp")
+    @ResponseBody
+    public ResultFul<?> ftpUpload(MultipartFile file) throws IOException {
+        return fileService.ftpUpload(file);
+    }
 
     @AuthAccess(desc = "删除文件")
     @DeleteMapping("/delete")
@@ -44,5 +47,5 @@ public class FileController {
         return fileService.delete(imagePath);
     }
 
-    ;
+
 }
