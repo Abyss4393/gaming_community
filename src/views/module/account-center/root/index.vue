2e9a7c21932bc668d0b7ab9a76a21f1ca8a03dea
wql <2294336102@qq.com>
2023-12-02 20:06:55 +0800

    完善新功能

--- src/views/module/account-center/root/index.vue
+++ src/views/module/account-center/root/index.vue
@@ -3,24 +3,27 @@
         <div class="account-container">
             <div class="account-header">
                 <div class="avatar">
-                    <img :src="userInfo.avatar" alt="">
+                    <img :src="data.rederUserInfo.avatar" alt="avatar">
                 </div>
                 <div class="account-details">
                     <div class="nickname">
-                        <h2>{{ userInfo.nickname }}</h2>
-                        <el-button type="primary">subescribe</el-button>
+                        <h2>{{ data.rederUserInfo.nickname }}</h2>
+                        <el-button v-if="current_id == userInfo.id" type="primary" @click="edit()">编辑</el-button>
+                        <el-button v-else type="primary" @click="follow()">
+                            {{ data.isAttented ? '√已关注' : '关注' }}
+                        </el-button>
                     </div>
                     <div class="gender">
-                        {{ userInfo.gender }}
+                        {{ data.rederUserInfo.gender }}
                     </div>
                     <div class="mobile">
-                        {{ userInfo.mobile }}
+                        {{ data.rederUserInfo.mobile }}
                     </div>
                     <div class="email">
-                        {{ userInfo.email }}
+                        {{ data.rederUserInfo.email }}
                     </div>
                     <div class="login">
-                        最近登录时间：{{ userInfo.lastLandingTime }}
+                        最近登录时间：{{ data.rederUserInfo.lastLandingTime }}
                     </div>
                 </div>
             </div>
@@ -31,14 +34,23 @@
                         个人中心
                     </div>
                     <div class="menu">
-                        <ul>
-                            <li class="menu-item" v-for="item, index in data.menuList" :key="index"
-                                @click="loginout(index)">
+                        <ul v-if="current_id == userInfo.id">
+                            <li class="menu-item" v-for="item, index in data.menuList" :key="index" @click="active(index)">
                                 <a :href="item.herf === '' ? null : item.herf" @mouseenter="onMouseenter(item)"
                                     @mouseleave="onMouseleave(item)">
                                     <img :src="choose(item)" alt="">
-                                    <span>{{ item.text }}
-                                    </span></a>
+                                    <span>{{ item.text }}</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul v-else>
+                            <li class="menu-item" v-for="currnetItem, currnetIndex in data.currentMenuList"
+                                :key="currnetIndex">
+                                <a :href="currnetItem.herf === '' ? null : currnetItem.herf"
+                                    @mouseenter="onMouseenter(currnetItem)" @mouseleave="onMouseleave(currnetItem)">
+                                    <img :src="choose(currnetItem)" alt="">
+                                    <span>{{ currnetItem.text }}</span>
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -52,67 +64,133 @@
 </template>
 <script setup>
 import { useStore } from 'vuex';
-import { ElCard, ElButton } from 'element-plus';
-import { computed, reactive, getCurrentInstance } from 'vue';
+import { ElCard, ElButton, ElMessage } from 'element-plus';
+import { computed, reactive, getCurrentInstance, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { UserInfo, AddFriend } from "@/utils/request/common.js";
 
 const store = useStore();
 const instance = getCurrentInstance();
+const current_id = useRoute().query.id;
 const userInfo = useStore().getters['user/getUserInfo'].data;
+
 
 const data = reactive({
     menuList: [{
         img: require('@/assets/static/icons/post.png'),
         active: require('@/assets/static/icons/post-active.png'),
         text: '我的帖子',
-        herf: `/abyss/accountCenter/postList?author_id=${userInfo.id}`,
-        hover: false
+        herf: `/abyss/accountCenter/postList?id=${userInfo.id}`,
+        hover: false,
+
     }, {
         img: require('@/assets/static/icons/reply.png'),
         active: require('@/assets/static/icons/reply-active.png'),
         text: '我的回复',
-        herf: `/abyss/accountCenter/replyList?author_id=${userInfo.id}`,
-        hover: false
+        herf: `/abyss/accountCenter/replyList?id=${userInfo.id}`,
+        hover: false,
+
     },
     {
         img: require('@/assets/static/icons/comment.png'),
         active: require('@/assets/static/icons/comment-active.png'),
         text: '我的评论',
-        herf: `/abyss/accountCenter/comments?author_id=${userInfo.id}`,
-        hover: false
+        herf: `/abyss/accountCenter/comments?id=${userInfo.id}`,
+        hover: false,
+
     },
     {
         img: require('@/assets/static/icons/center-collection.png'),
         active: require('@/assets/static/icons/center-collection-active.png'),
         text: '我的收藏',
-        herf: `/abyss/accountCenter/collections?author_id=${userInfo.id}`,
-        hover: false
+        herf: `/abyss/accountCenter/collections?id=${userInfo.id}`,
+        hover: false,
+
     },
     {
         img: require('@/assets/static/icons/user.png'),
         active: require('@/assets/static/icons/user-active.png'),
         text: '编辑资料',
-        herf: `/abyss/accountCenter/edit?author_id=${userInfo.id}`,
-        hover: false
+        herf: `/abyss/accountCenter/edit?id=${userInfo.id}`,
+        hover: false,
+
     },
     {
         img: require('@/assets/static/icons/loginout.png'),
         active: require('@/assets/static/icons/loginout-active.png'),
         text: '退出登录',
         herf: '',
-        hover: false
-    }]
+        hover: false,
+    }],
+    currentMenuList: [
+        {
+            img: require('@/assets/static/icons/post.png'),
+            active: require('@/assets/static/icons/post-active.png'),
+            text: '帖子',
+            herf: `/abyss/accountCenter/postList?id=${current_id}`,
+            hover: false,
+
+        }, {
+            img: require('@/assets/static/icons/reply.png'),
+            active: require('@/assets/static/icons/reply-active.png'),
+            text: '回复',
+            herf: `/abyss/accountCenter/replyList?id=${current_id}`,
+            hover: false,
+        }, {
+            img: require('@/assets/static/icons/comment.png'),
+            active: require('@/assets/static/icons/comment-active.png'),
+            text: '评论',
+            herf: `/abyss/accountCenter/comments?id=${current_id}`,
+            hover: false,
+        },
+        {
+            img: require('@/assets/static/icons/center-collection.png'),
+            active: require('@/assets/static/icons/center-collection-active.png'),
+            text: '收藏',
+            herf: `/abyss/accountCenter/collections?id=${current_id}`,
+            hover: false,
+        }, {
+            img: require('@/assets/static/icons/attention.png'),
+            active: require('@/assets/static/icons/attention-active.png'),
+            text: '关注',
+            herf: '',
+            hover: false,
+        },],
+    rederUserInfo: {},
+    isAttented: false
+})
+
+onMounted(async function init() {
+    if (current_id == userInfo.id) {
+        data.rederUserInfo = userInfo;
+        return
+    }
+    const res = await UserInfo(current_id)
+    if (res.meta.code === 200)
+        data.rederUserInfo = res.data
 })
 
 const onMouseenter = (item) => item.hover = true
 const onMouseleave = (item) => item.hover = false
 const choose = computed(() => {
     return (item) => {
-        if (item.hover) return item.active
+        if (item.hover) return item.active;
         return item.img
     }
 })
 
-const loginout = (index) => {
+const follow = async () => {
+    const res = await AddFriend(userInfo.id, current_id)
+    console.log(res);
+    if (res.meta.code === 271)
+        ElMessage.success(res.meta.msg)
+    else ElMessage.error(res.meta.msg)
+}
+
+const edit = () => instance.proxy.$router.push(`/abyss/accountCenter/edit?id=${userInfo.id}`)
+
+const active = (index) => {
+    data.menuList[index].focus = true;
     if (index === data.menuList.length - 1) {
         store.commit('user/resetUserInfo');
         location.reload();
